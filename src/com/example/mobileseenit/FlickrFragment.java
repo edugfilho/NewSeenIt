b23b1de48b8282e5c0dd63e5b34da8cd248f7586
drunkel <dylan.runkel@gmail.com>
2013-11-09 12:51:52 -0400

    Load and show multiple pictures...still of dogs.

--- src/com/example/mobileseenit/FlickrFragment.java
+++ src/com/example/mobileseenit/FlickrFragment.java
@@ -17,22 +17,21 @@ import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
 
 import com.example.mobileseenit.apis.FlickrSearchTask;
 
 public class FlickrFragment extends Fragment implements OnTouchListener,
 		OnClickListener {
-
+	
 	private static final String TAG = "FlickrFragment";
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_flickr, container,
-				false);
-		 Button b = (Button) v.findViewById(R.id.searchFlickr);
-	        b.setOnClickListener(this);
+		View v = inflater.inflate(R.layout.fragment_flickr, container, false);
+		Button b = (Button) v.findViewById(R.id.searchFlickr);
+		b.setOnClickListener(this);
 		return v;
 	}
 
@@ -45,37 +44,32 @@ public class FlickrFragment extends Fragment implements OnTouchListener,
 		if (networkInfo != null && networkInfo.isConnected()) {
 			FlickrSearchTask searchTask = new FlickrSearchTask(this);
 			searchTask.execute();
-			
-			//get photos
-			Log.i(TAG,"Photos Loaded");
-			
+
+			// get photos
+			Log.i(TAG, "Photos Loaded");
+
 		} else {
 			Log.i(TAG, "No network connection available.");
 		}
 	}
+
+	public void displayPhotos(ArrayList<Bitmap> photos) {
 	
-	public void displayPhotos(ArrayList<Bitmap> photos)
-	{
-		for(Bitmap p : photos)
-		{
+		for (Bitmap p : photos) {
 			ImageView image = new ImageView(getActivity());
 			image.setImageBitmap(p);
-			image.setX(100);
-			image.setY(100);
-			RelativeLayout r = (RelativeLayout)getActivity().findViewById(R.id.flickr_relative_layout);
+			LinearLayout r = (LinearLayout) getActivity().findViewById(
+					R.id.flickr_layout);
 			r.addView(image);
-			return;
 		}
 	}
-	
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-        case R.id.searchFlickr:
-        	searchFlickr(v);
+		case R.id.searchFlickr:
+			searchFlickr(v);
 		}
-
 
 	}
 
