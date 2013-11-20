dabb9e3285de0da0fd204ddf434f7703cd153ec3
drunkel <dylan.runkel@gmail.com>
2013-11-20 13:54:08 -0400

    Code cleanup, documentation

--- src/com/example/mobileseenit/apis/FlickrTestAuthTask.java
+++ src/com/example/mobileseenit/apis/FlickrTestAuthTask.java
@@ -3,107 +3,97 @@ package com.example.mobileseenit.apis;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 import javax.xml.parsers.ParserConfigurationException;
-
 import org.xml.sax.SAXException;
-
-import android.app.DialogFragment;
 import android.os.AsyncTask;
-
 import com.aetrion.flickr.Flickr;
 import com.aetrion.flickr.FlickrException;
-import com.aetrion.flickr.REST;
 import com.aetrion.flickr.RequestContext;
 import com.aetrion.flickr.auth.Auth;
 import com.aetrion.flickr.auth.AuthInterface;
 import com.example.mobileseenit.MainActivity;
 
-//Code based off Example
-//https://github.com/r0man/flickrj/blob/master/examples/AuthExample.java
-public class FlickrTestAuthTask extends AsyncTask<String, Void, String>{
+/**
+ * Async task to finish the Auth process for Flickr. There should be a better
+ * way to do this (ie. in just the loginTask), but we need the user to complete
+ * the login page and exit the dialog before we can proceed. In order to do this
+ * simply, the 2 parts of the authorization were broken up (FlickrLoginTask and
+ * this class).
+ * 
+ * Based off example here:
+ * https://github.com/r0man/flickrj/blob/master/examples/AuthExample.java
+ * 
+ * @author dylanrunkel
+ * 
+ */
+public class FlickrTestAuthTask extends AsyncTask<String, Void, String> {
 
 	Flickr f;
 	RequestContext requestContext;
 	String frob = "";
 	String token = "";
-	
+
 	FlickrLoginDialog fragment;
 	AuthInterface a;
 	MainActivity mainActivity;
 	Auth auth;
-	
+
 	public FlickrTestAuthTask(FlickrLoginDialog gg, AuthInterface a) {
-		
-		fragment =  gg;
-		mainActivity  = (MainActivity) gg.getActivity();
-	
+
+		fragment = gg;
+		mainActivity = (MainActivity) gg.getActivity();
+
 		f = mainActivity.getFlickr();
-		this.a =f.getAuthInterface();
+		this.a = f.getAuthInterface();
 	}
-	
+
 	@Override
 	protected String doInBackground(String... searchTerms) {
-
-		// params comes from the execute() call. Will be the search terms.
 		try {
-				return search(searchTerms[0]);
-
-		} catch (IOException e) {
-			return "Unable to retrieve web page. URL may be invalid.";
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
+			return search(searchTerms[0]);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		return "done";
+		return null;
 	}
-	public String search(String... frobString) throws IOException, SAXException, ParserConfigurationException {
 
-		BufferedReader infile =
-		          new BufferedReader ( new InputStreamReader (System.in) );
-		        String line = infile.readLine();
-		        try {
-		        	frob = frobString[0];
-		            auth = f.getAuthInterface().getToken(frob);
-		            f.setAuth(auth);
-		            System.out.println("Authentication success");
-		            // This token can be used until the user revokes it.
-		            System.out.println("Token: " + auth.getToken());
-		            System.out.println("nsid: " + auth.getUser().getId());
-		            System.out.println("Realname: " + auth.getUser().getRealName());
-		            System.out.println("Username: " + auth.getUser().getUsername());
-		            System.out.println("Permission: " + auth.getPermission().getType());
-		            
-		            
-		        } catch (FlickrException e) {
-		            System.out.println("Authentication failed");
-		            e.printStackTrace();
-		        }
+	
+	public String search(String... frobString) throws IOException,
+			SAXException, ParserConfigurationException {
+
+		BufferedReader infile = new BufferedReader(new InputStreamReader(
+				System.in));
+		String line = infile.readLine();
+		try {
+			frob = frobString[0];
+			auth = f.getAuthInterface().getToken(frob);
+			f.setAuth(auth);
+			System.out.println("Authentication success");
+			// This token can be used until the user revokes it.
+			System.out.println("Token: " + auth.getToken());
+			System.out.println("nsid: " + auth.getUser().getId());
+			System.out.println("Realname: " + auth.getUser().getRealName());
+			System.out.println("Username: " + auth.getUser().getUsername());
+			System.out.println("Permission: " + auth.getPermission().getType());
+
+		} catch (FlickrException e) {
+			System.out.println("Authentication failed");
+			e.printStackTrace();
+		}
 
 		return "test";
 	}
-	
-	
-	
 
-	
-//	// onPostExecute displays the results of the AsyncTask.
+	// onPostExecute displays the results of the AsyncTask.
 	@Override
 	protected void onPostExecute(String result) {
-		  //Set stuff
-        FlickrUser newUser = new FlickrUser();
-        newUser.setUsername(auth.getUser().getUsername());
-        
-        fragment.updateFlickr(f, "");
-        fragment.acceptFlickrUser(newUser);
-        System.out.println();
+		// Set stuff
+		FlickrUser newUser = new FlickrUser();
+		newUser.setUsername(auth.getUser().getUsername());
+
+		fragment.updateFlickr(f, "");
+		fragment.acceptFlickrUser(newUser);
+		System.out.println();
 	}
-	
-	
-	
 
 }
--- src/com/example/mobileseenit/apis/FlickrUploadTask.java
+++ src/com/example/mobileseenit/apis/FlickrUploadTask.java
@@ -1,69 +1,84 @@
 package com.example.mobileseenit.apis;
 
 import java.io.IOException;
-
 import javax.xml.parsers.ParserConfigurationException;
-
 import org.xml.sax.SAXException;
-
 import android.os.AsyncTask;
-
 import com.aetrion.flickr.Flickr;
 import com.aetrion.flickr.RequestContext;
-import com.aetrion.flickr.auth.Permission;
 import com.aetrion.flickr.uploader.UploadMetaData;
 
+/**
+ * Async task used to upload a photo with metadata to Flickr. Uses the FlickrJ
+ * library.
+ * 
+ * @author dylanrunkel
+ * 
+ */
 public class FlickrUploadTask extends AsyncTask<String, Void, String> {
 
+	// Flickrj object
 	Flickr f;
 
-	byte data[] ;
+	// Data array of photo (done in fragment before calling)
+	byte data[];
+
+	// Any metadata (title etc) added to the image. Done in fragment.
 	UploadMetaData meta;
+
+	// Required for upload request
 	RequestContext requestContext;
 
-	
-	public FlickrUploadTask(Flickr flickrObject, byte data[], UploadMetaData m) {
-
-		
+	/**
+	 * Constructor to build this upload task. Calling classes must pass in all 3
+	 * parameters.
+	 * 
+	 * @param flickrObject
+	 *            - FlickrJ object from MainActivity
+	 * @param data
+	 *            - The preprocessed byte array of the photo to upload
+	 * @param metaData
+	 *            - meta data about the photo.
+	 */
+	public FlickrUploadTask(Flickr flickrObject, byte data[],
+			UploadMetaData metaData) {
 		this.data = data;
-		this.meta = m;
+		this.meta = metaData;
 		this.f = flickrObject;
 	}
 
 	@Override
 	protected String doInBackground(String... params) {
-		// params comes from the execute() call. Will be the search terms.
 		try {
 			return upload(params);
-
-		} catch (IOException e) {
-			return "Unable to retrieve web page. URL may be invalid.";
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
+	/**
+	 * Actual upload logic. Prepares the request context with our Flickr Auth,
+	 * and attempts the upload.
+	 * 
+	 * @param searchTerms
+	 * @return
+	 * @throws IOException
+	 * @throws SAXException
+	 * @throws ParserConfigurationException
+	 */
 	public String upload(String... searchTerms) throws IOException,
 			SAXException, ParserConfigurationException {
-		
+
 		requestContext = RequestContext.getRequestContext();
 		requestContext.setAuth(f.getAuth());
 
 		try {
-			f.getAuth().setPermission(Permission.WRITE);
 			f.getUploader().upload(data, meta);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-	
-
-		return "done";
+		return null;
 	}
 
 }
